36104de82446b10333b4ef3c292b2f175d92f359
Andy Likuski <andy@likuski.org>
2017-06-09 19:19:52 -0700

    second commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import {run} from '@cycle/run'
+import {makeDOMDriver, h1} from '@cycle/dom';
+import {App} from './app'
+import xs from 'xstream';
+
+function main() {
+    const sinks = {
+        DOM: xs.periodic(1000).map(i =>
+            h1('' + i + ' seconds elapsed')
+        )
+    };
+    return sinks;
+}
+
+const drivers = {
+  DOM: makeDOMDriver('#app')
+}
+
+run(main, drivers)
